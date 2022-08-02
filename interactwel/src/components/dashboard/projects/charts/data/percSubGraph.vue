3be00ce4ef4db7940b26046c78f40b38058f9637
codexpotech <43480518+codexpotech@users.noreply.github.com>
2022-08-02 21:18:35 +0530

    Update percSubGraph.vue

--- interactwel/src/components/dashboard/projects/charts/data/percSubGraph.vue
+++ interactwel/src/components/dashboard/projects/charts/data/percSubGraph.vue
@@ -29,7 +29,7 @@ export default {
       JSONData: null,
       datacollection: null,
       graphColors: [
-        "#007be9",
+        "#28a745",
       ],
       options: {
         responsive: true,
